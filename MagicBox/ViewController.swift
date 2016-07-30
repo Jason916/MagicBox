fde48da4182892c4789fc2c944b5bcdb35a052ca
JasonXu <jasonxu@jasonxudemacbook-pro.local>
2016-07-30 15:03:26 +0800

    Initial Commit

---
+++ MagicBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MagicBox
+//
+//  Created by JasonXu on 16/7/30.
+//  Copyright © 2016年 JasonXu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
